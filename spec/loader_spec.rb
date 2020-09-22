788da1a1ff9dbba1e28394825819e92a79873c3f
Mikhail Grishko <mikhail.grishko@gmail.com>
2020-09-22 18:10:51 +0300

    add spec for broken data

--- spec/loader_spec.rb
+++ spec/loader_spec.rb
@@ -12,8 +12,18 @@ RSpec.describe Parser::Loader do
   end
 
   describe '#call' do
-    it 'returns array of Parser::Models::LogRec instances' do
-      expect(subject.call).to all be_kind_of Parser::Models::LogRec
+    context 'when data is correct' do
+      it 'returns array of Parser::Models::LogRec instances' do
+        expect(subject.call).to all be_kind_of Parser::Models::LogRec
+      end
+    end
+
+    context 'when data is broken' do
+      let(:parsed_file) { File.join(Dir.pwd, 'spec', 'fixtures', 'webserver_broken_data.log') }
+
+      it 'returns array of Parser::Models::LogRec instances' do
+        expect { subject.call }.to raise_error(Parser::Errors::PathError)
+      end
     end
   end
 end
